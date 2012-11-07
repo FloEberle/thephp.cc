b66da2c9ec53cfa85d868b05d0140dfa8b45a832
Flo Eberle <florian.eberle@competec.ch>
2012-11-07 17:28:05 +0100

    Aufgabe 2: Work in progress

---
+++ solutions_flo/2/Isbn.php
@@ -0,0 +1,15 @@
+<?php
+final class Isbn
+{
+    /**
+     * @var int
+     */
+    private $isbn;
+    
+    public function __construct($isbn)
+    {
+        
+    }
+    
+    // http://php.net/manual/en/language.oop5.object-comparison.php
+}
