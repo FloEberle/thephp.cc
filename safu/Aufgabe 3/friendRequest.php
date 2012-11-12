4ecf5dd66c5410f06266b59ccdebbdbbaefedc5d
Samuel Furter <samuel.furter@brack.ch>
2012-11-12 17:28:38 +0100

    friendrequest methode erstellt

--- safu/Aufgabe 3/friendRequest.php
+++ safu/Aufgabe 3/friendRequest.php
@@ -2,10 +2,28 @@
 
 class User
 {
-      
+    
+    public function __construct(){
+        $this->userName = $userName;
+        $this->friends = array(); 
+    }
+    
+    
+    /**
+     * @param object $friendRequest
+     */  
     public function addFriendRequest(FriendRequest $friendRequest)
     {
+       /*
+        * Kontrollieren ob man sich selber added oder ob der Freund schon bereits in der Liste vorhanden ist. 
+        * */
+        if($friendRequest['from'] == $this->userName){
+            throw new Exception('Sie können sich selbst nicht als Freund hinzufügen.');
+        }
         
+        if(in_array($friendRequest['from'], $this->friends)){
+            throw new Exception('Sie sind bereits schon ein Freund von: '. $this->userName);
+        }
     }
     
     public function confirm(FriendRequest $friendRequest)
