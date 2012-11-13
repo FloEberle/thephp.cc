71dc49d445bd1ff5cb1a99b74c8e569e04907e54
Stefan Priebsch <stefan@priebsch.de>
2012-11-13 12:21:33 +0100

    Erster Unit-Test.

---
+++ training_2/aufgabe-1/src/User.php
@@ -0,0 +1,69 @@
+<?php
+
+class User
+{
+	private $id;
+	private $realName;
+	private $email;
+	private $screenName;
+
+	public function __construct($id, $email, $realName, $screenName = null)
+	{
+		$this->id = $id;
+		$this->setEmail($email);
+		$this->realName = $realName;
+
+		if ($screenName !== null) {
+			$this->setScreenName($screenName);
+        }
+	}
+
+    /**
+     * @return string
+     */
+	public function getRealName()
+	{
+		return $this->realName;
+	}
+
+    /**
+     * @param string $email
+     */
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
+
+    /**
+     * @return string
+     */
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+    /**
+     * @param string $screenName
+     */
+	public function setScreenName($screenName)
+	{
+		$this->screenName=$screenName;
+	}
+
+    /**
+     * @return string
+     */
+	public function getScreenName()
+	{
+        if ($this->screenName === null) {
+            return $this->getRealName();
+        }
+
+        return $this->screenName;
+	}
+
+    public function deleteScreenName()
+    {
+        $this->screenName = null;
+    }
+}
