1fba5bca0a9e94d6d040fbd046f26066270bc551
sepeter <sebastian.peter@competec.ch>
2012-11-15 13:48:22 +0100

    Aufgabe 3 verbessert + PHPUnit Tests

--- sepe/3/src/User.php
+++ sepe/3/src/User.php
@@ -2,46 +2,83 @@
 
 class User
 {
+    /**
+     * @var array
+     */
     private $friends = array();
+
+    /**
+     * @var array
+     */
     private $requests = array();
+
+    /**
+     * @var array
+     */
     private $declined = array();
 
+    /**
+    * @var String
+    */
     private $userName;
 
+    /**
+     * @param FriendRequest $friendRequest
+     * @throws InvalidArgumentException
+     */
     public function addFriendRequest(FriendRequest $friendRequest)
     {
-        if (empty($friendRequest)) {
-            throw new InvalidArgumentException('No Parameter..');
+        if (array_search($friendRequest->getFrom(),$this->friends)==true) {
+            throw new InvalidArgumentException('Friend still in Friendslist ..');
         }
         $this->requests[$friendRequest->getId()] = $friendRequest->getFrom();
     }
+
+    /**
+     * @param FriendRequest $friendRequest
+     * @throws InvalidArgumentException
+     */
     public function confirm(FriendRequest $friendRequest)
     {
-        if (empty($friendRequest)) {
-            throw new InvalidArgumentException('No Parameter..');
+        if ($friendRequest->getTo() !== $this) {
+            throw new InvalidArgumentException('FriendRequest is not for this User..');
         }
         $this->friends[$friendRequest->getId()] = $friendRequest->getFrom()->getUserName();
         unset($this->requests[$friendRequest->getId()]);
         $friendRequest->getFrom()->friends[$friendRequest->getId()] = $friendRequest->getTo()->getUserName();
     }
+
+    /**
+     * @param FriendRequest $friendRequest
+     * @throws InvalidArgumentException
+     */
     public function decline(FriendRequest $friendRequest)
     {
-        if (empty($friendRequest)) {
-            throw new InvalidArgumentException('No Parameter..');
+        if ($friendRequest->getTo() !== $this) {
+            throw new InvalidArgumentException('FriendRequest is not for this User..');
         }
         $this->declined[$friendRequest->getId()] = $friendRequest->getFrom()->getUserName();
         unset($this->requests[$friendRequest->getId()]);
     }
+
+    /**
+     * @param User $friend
+     * @throws InvalidArgumentException
+     */
     public function removeFriend(User $friend)
     {
-        if (empty($friend)) {
-            throw new InvalidArgumentException('No Parameter..');
+        if (array_search($friend->getUserName(),$this->friends)==false) {
+            throw new InvalidArgumentException('Not a Friend for this User..');
         }
         $pos = array_search($friend->getUserName(),$this->friends);
         unset($this->friends[$pos]);
         $pos = array_search($this->getUserName(),$friend->friends);
         unset($friend->friends[$pos]);
     }
+
+    /**
+     * @param $userName
+     */
     public function setUserName($userName)
     {
         $this->userName = $userName;
