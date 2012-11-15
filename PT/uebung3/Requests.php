69571e68d96c2134429945a1e03ae7d9784a4a54
cnphyt <patrick.thieme@competec.ch>
2012-11-15 09:55:51 +0100

    Subscription implementiert (einseite Freundschaft), Tests erweitert

--- PT/uebung3/Requests.php
+++ PT/uebung3/Requests.php
@@ -21,3 +21,25 @@ class FriendRequest
         return $this->to;
     }
 }
+
+class SubscriptionRequest
+{
+    private $from;
+    private $to;
+
+    public function __construct(User $from, User $to)
+    {
+        $this->from = $from;
+        $this->to = $to;
+    }
+
+    public function getFrom()
+    {
+        return $this->from;
+    }
+
+    public function getTo()
+    {
+        return $this->to;
+    }
+}
