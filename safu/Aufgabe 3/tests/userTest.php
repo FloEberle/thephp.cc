beea04b772bc5c5b1f76ee67ae4354fbc5ad5b81
Samuel Furter <samuel.furter@brack.ch>
2012-11-20 09:31:43 +0100

    PHP Unit Test versucht zum laufen zu bringen ging aber nicht

--- safu/Aufgabe 3/tests/userTest.php
+++ safu/Aufgabe 3/tests/userTest.php
@@ -1,34 +1,45 @@
 <?php
-
- class userTest extends PHPUnit_Framework_TestCase
+ class UserTest extends PHPUnit_Framework_TestCase
 {
-   
+   /**
+    * @var $friendRequest Object
+    */
+   private $friendRequest;
 
+   /**
+    * @var $setUser1 Object
+    */
+   private $setUser1;
+   
+   /**
+    * @var $setUser2 Object
+    */
+   private $setUser2;
+   
     protected function setUp()
     {
-        $setUser1 = new User("Samuel");
-	$setUser2 = new User("Hugo");
-    	$friendRequest = new FriendRequest($setUser1, $setUser2);
+        $this->setUser1 = new User("Samuel");
+	$this->setUser2 = new User("Hugo");
+    	$this->friendRequest = new FriendRequest($this->setUser1, $this->setUser2);
     }
 
-    public function testAddingAnFriendRequestWorks ()
+    public function testAddingAnFriendRequestWorks()
     {
-        $this->assertEquals('', $setUser2-> addFriendRequest($friendRequest));
+        $this->assertEquals('', $this->setUser2->addFriendRequest($this->friendRequest));
     }
 
     public function testConfirmAnFriendWorks()
     {
-	$this->assertEquals('true', $setUser2->confirm($friendRequest));
+	$this->assertEquals('true', $this->setUser2->confirm($this->friendRequest));
     }
 
     public function testDeclineAnFriendWorks()
     {
-	$this->assertEquals('$friendRequest == null', $setUser1->decline($friendRequest));
+	$this->assertEquals('$this->friendRequest == null', $this->setUser1->decline($this->friendRequest));
     }
 
     public function testRemoveAnFriendWorks()
     {
-	$this->assertEquals('true', $setUser1->removeFriend('Hugo', $setUser1));
+	$this->assertEquals('true', $this->setUser1->removeFriend('Hugo', $this->setUser1));
     }
-
-
+}
