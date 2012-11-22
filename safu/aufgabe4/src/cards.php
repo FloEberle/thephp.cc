8cfd8c22075e6769c24e9168304ebcf967e1c474
Samuel Furter <samuel.furter@brack.ch>
2012-11-22 15:45:07 +0100

    Cards erweiteret aber immernoch im test stadium

--- safu/aufgabe4/src/cards.php
+++ safu/aufgabe4/src/cards.php
@@ -1,7 +1,53 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+class cards 
+{
+    /**
+     * @var $player Object
+     */
+    private $player; 
+    
+    /**
+     * @var $rand integer
+     */
+    private $rand;
+    
+    public function __construct (player $player) 
+    {
+      $this->player = $player;
+    }
+    
+    private function setCards ()
+    {
+        $this->rand = $this->randomCard();
+        
+        if($this->hasCard($this->rand)){
+            while(!$this->hasCard($this->randomCard()))
+            {
+                
+            }
+        }
+      
+       
+    }
+    
+    public function getCards ()
+    {        
+      return $this->player->cards;
+    }
+    
+    private function randomCard ()
+    {
+       return $this->$rand = rand(1, 6); 
+    }
+    
+    public function hasCard ($number)
+    {
+        if (array_key_exists($number, $this->player->cards)){
+            return $this->player->cards[$number] = true;
+        }
+        
+        return false;
+    }
+    
+}
