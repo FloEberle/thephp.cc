56af197c285c462bc230c0539a806bec68ab91ff
sepeter <sebastian.peter@competec.ch>
2012-11-30 15:31:54 +0100

    Tests angefangen

--- sepe/4/tests/CardSetTest.php
+++ sepe/4/tests/CardSetTest.php
@@ -2,8 +2,27 @@
 
 class CardGeneratorTest extends PHPUnit_Framework_TestCase
 {
+    private $cardSet;
+    private $colors = [
+        'red',
+        'blue',
+        'yellow',
+        'green',
+        'black',
+        'white'
+    ];
 
     protected function setUp()
+    {
+        $this->cardSet = new CardSet($this->colors);
+    }
+
+    public function testGetCardSetWorks()
+    {
+
+    }
+
+    public function testHasColorWorks()
     {
 
     }
